2d000d6e289a81c4b1690f913bd5672fc53d3722
postbire3440 <36615719+postbire3440@users.noreply.github.com>
2018-02-19 21:47:55 -0500

    Add files via upload

---
+++ Distance Traveled.py
@@ -0,0 +1,37 @@
+Python 3.6.4 (v3.6.4:d48ecebad5, Dec 18 2017, 21:07:28) 
+[GCC 4.2.1 (Apple Inc. build 5666) (dot 3)] on darwin
+Type "copyright", "credits" or "license()" for more information.
+>>> WARNING: The version of Tcl/Tk (8.5.9) in use may be unstable.
+Visit http://www.python.org/download/mac/tcltk/ for current information.
+
+== RESTART: /Users/Postbird/Documents/P2HW_DistanceTraveled_PostBirdErin.py ==
+#CTI-110
+#P2HW1-Distance Traveled
+#Erin
+#2/18/2018
+#
+>>> 
+== RESTART: /Users/Postbird/Documents/P2HW_DistanceTraveled_PostBirdErin.py ==
+#CTI-110
+#P2HW1-Distance Traveled
+#Erin
+#2/18/2018
+#
+Input a speed and distance
+speed:70mph
+== RESTART: /Users/Postbird/Documents/P2HW_DistanceTraveled_PostBirdErin.py ==
+#CTI-110
+#P2HW1-Distance Traveled
+#Erin
+#2/18/2018
+#
+Input a speed and distance
+speed:70mph
+== RESTART: /Users/Postbird/Documents/P2HW_DistanceTraveled_PostBirdErin.py ==
+#CTI-110
+#P2HW1-Distance Traveled
+#Erin
+#2/18/2018
+#
+Input a rate and distance
+rate:70mph
